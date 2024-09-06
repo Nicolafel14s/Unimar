74f21504c56ca194f5e2cc29bae081b7cb9e08e8
Nicolafel14s <nicolasfelipesantossilva1@gmail.com>
2024-09-06 18:59:32 -0300

    commit initial

---
+++ main.js
@@ -0,0 +1,48 @@
+function selecionar_imagem1() {
+    document.getElementById('id3').style.filter = "grayscale(100%)"
+    document.getElementById('id4').style.filter = "grayscale(100%)"
+    document.getElementById('id5').style.filter = "grayscale(100%)"
+    document.getElementById('id6').style.filter = "grayscale(100%)"
+    document.getElementById('id1').style.filter = "grayscale(0%)"
+    document.getElementById('id2').style.filter = "grayscale(100%)"
+  }
+  function selecionar_imagem2() {
+    document.getElementById('id3').style.filter = "grayscale(100%)"
+    document.getElementById('id4').style.filter = "grayscale(100%)"
+    document.getElementById('id5').style.filter = "grayscale(100%)"
+    document.getElementById('id6').style.filter = "grayscale(100%)"
+    document.getElementById('id1').style.filter = "grayscale(100%)"
+    document.getElementById('id2').style.filter = "grayscale(0%)"
+  }
+  function selecionar_imagem3() {
+    document.getElementById('id3').style.filter = "grayscale(0%)"
+    document.getElementById('id4').style.filter = "grayscale(100%)"
+    document.getElementById('id5').style.filter = "grayscale(100%)"
+    document.getElementById('id6').style.filter = "grayscale(100%)"
+    document.getElementById('id1').style.filter = "grayscale(100%)"
+    document.getElementById('id2').style.filter = "grayscale(100%)"
+  }
+  function selecionar_imagem4() {
+    document.getElementById('id3').style.filter = "grayscale(100%)"
+    document.getElementById('id4').style.filter = "grayscale(0%)"
+    document.getElementById('id5').style.filter = "grayscale(100%)"
+    document.getElementById('id6').style.filter = "grayscale(100%)"
+    document.getElementById('id1').style.filter = "grayscale(100%)"
+    document.getElementById('id2').style.filter = "grayscale(100%)"
+  }
+  function selecionar_imagem5() {
+    document.getElementById('id3').style.filter = "grayscale(100%)"
+    document.getElementById('id4').style.filter = "grayscale(100%)"
+    document.getElementById('id5').style.filter = "grayscale(0%)"
+    document.getElementById('id6').style.filter = "grayscale(100%)"
+    document.getElementById('id1').style.filter = "grayscale(100%)"
+    document.getElementById('id2').style.filter = "grayscale(100%)"
+  }
+  function selecionar_imagem6() {
+    document.getElementById('id3').style.filter = "grayscale(100%)"
+    document.getElementById('id4').style.filter = "grayscale(100%)"
+    document.getElementById('id5').style.filter = "grayscale(100%)"
+    document.getElementById('id6').style.filter = "grayscale(0%)"
+    document.getElementById('id1').style.filter = "grayscale(100%)"
+    document.getElementById('id2').style.filter = "grayscale(100%)"
+  }
